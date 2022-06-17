f874ec43665ae90dae383427f91bc4199aec7c16
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-17 08:30:03 -0300

    portfolio thiago

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,19 @@
-import React from 'react';
 import Footer from '../components/Footer';
+import styles from '../styles/Contact.module.css';
 
 function Contact() {
   return (
-    <div>
+    <div className={ styles.container }>
       <h1>Contato</h1>
+      <form action="https://formsubmit.co/t.fs.2007@hotmail.com" method="POST">
+        <label htmlFor="name">Nome</label>  
+        <input type="text" name="name" id="name" />
+        <label htmlFor="email">E-mail</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="message">Mensagem</label>
+        <textarea name="message" id="message" cols="30" rows="10" />
+        <button type="submit">Enviar</button>
+      </form>
       <Footer />
     </div>
   )
